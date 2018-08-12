2e7bf8a004cc12246f92126afb9d3eafab1bb9ef
mantrid15 <40058139+mantrid15@users.noreply.github.com>
2018-08-12 17:31:39 +0300

    08_ternary

---
+++ 08_ternary/js/script.js
@@ -0,0 +1,18 @@
+console.log("!!!" + location);
+
+
+let  answer = prompt(("Введи число"), '10');
+answer = parseInt(answer);
+(answer >= 0) ? console.log(answer) : console.log(answer);
+
+
+
+/*
+const Image = document.querySelector('.imageBlock');
+let  answer = prompt("Сколько тебе лет?");
+answer = parseInt(answer);
+
+
+
+(answer >= 18) ?  Image.style.backgroundImage = 'url(img/pub.jpg)' : 	Image.style.backgroundImage = 'url(img/school.png)';
+*/
